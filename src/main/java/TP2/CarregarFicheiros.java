77782e31a271904c02f242cfceb3c9a956f19e3c
Rui Peixoto <1191587@isep.ipp.pt>
2020-11-30 01:06:29 +0000

    Ponto 1 - carregarFicheiros + criar grafos + testes

---
+++ src/main/java/TP2/CarregarFicheiros.java
@@ -0,0 +1,323 @@
+package TP2;
+
+import com.bahadirarslan.jeodezi.Coordinate;
+import com.bahadirarslan.jeodezi.GreatCircle;
+import graph.AdjacencyMatrixGraph;
+import graphbase.Graph;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import static TP2.Constants.*;
+
+public class CarregarFicheiros {                            //refactor par eliminar partes dos metodos repetidos?
+
+    private Graph<Country, String> countriesGraph;
+    private AdjacencyMatrixGraph<User, Double> usersGraph;
+    private String usersFilePath;
+    private String relationshipsFilePath;
+    private String countriesFilePath;
+    private String bordersFilePath;
+
+
+    /**
+     * Construtor
+     * De acordo com o parâmetro define os file paths para os ficheiros de texto a carregar.
+     * @param ficheirosPequenos True para carregar ficheiro pequenos.
+     *                          False para carregar os ficheiros grandes
+     */
+   public CarregarFicheiros(boolean ficheirosPequenos){
+
+       countriesGraph = new Graph<Country, String>(false);
+
+       if(ficheirosPequenos){
+           usersGraph = new AdjacencyMatrixGraph<>();
+
+           usersFilePath = S_USERS_PATH;
+           relationshipsFilePath = S_RELATIONSHIP_PATH ;
+           countriesFilePath = S_COUNTRIES_PATH ;
+           bordersFilePath = S_BORDERS_PATH;
+       }else{
+           usersGraph = new AdjacencyMatrixGraph<>(1000);
+
+           usersFilePath = BIG_USERS_PATH;
+           relationshipsFilePath = BIG_RELATIONSHIP_PATH;
+           countriesFilePath = BIG_COUNTRIES_PATH;
+           bordersFilePath = BIG_BORDERS_PATH;
+       }
+   }
+
+
+
+    /**
+     * Devolve um grafo, que utiliza mapa de adjacências, para representar uma rede de cidades.
+     * As capitais sao representadas como vertices e os ramos têm, como peso, as distâncias entre as capitais.
+     * @return Graph<Country, String>
+     */
+    public Graph<Country, String> getCountriesGraph() {
+        return countriesGraph;
+    }
+
+    /**
+     * Devolve um grago, que utiliza matriz de adjacencias,para representar uma rede social.
+     * Os utilizadores são representados como vertices e os ramos identificam uma relação de amizade entre utilizadores.
+     * @return AdjacencyMatrixGraph<User, Double>
+     */
+    public AdjacencyMatrixGraph<User, Double> getUsersGraph() {
+        return usersGraph;
+    }
+
+
+
+
+    /**
+     * Carrega utilizadores(vertices) e relações() para o grafo a partir dos ficheiros .txt
+     * @return AdjacencyMatrixGraph<User, Double> Grafo que utiliza matriz de adjacências
+     */
+   public AdjacencyMatrixGraph<User, Double> carregarGrafoUsers(){
+       usersGraph = new AdjacencyMatrixGraph<>();
+       uploadUsers();
+       uploadRelationships();
+       return getUsersGraph();
+   }
+
+    /**
+     * Carrega paises(vertices) e fronteiras(ramos) para o grafo a partir dos ficheiros .txt
+     * @return Graph<Country, String> Grafo que utiliza mapa de adjacências
+     */
+   public Graph<Country, String> carregarGrafoCountries(){
+       countriesGraph = new Graph<Country, String>(false);
+       uploadCountries();
+       uploadBorders();
+       return getCountriesGraph();
+   }
+
+
+
+
+
+
+
+    /**
+     * Preenche os vertices do grafo de utilizadores (matriz de adjacências).
+     * Carrega, a partir de um ficheiro de texto, uma lista de utilizadores que vão ser adicionados como vertices.
+     * @returnAdjacencyMatrixGraph<User,Double>
+     */
+    private AdjacencyMatrixGraph<User,Double> uploadUsers(){
+
+        int lineBeingRead = 0;
+        Scanner in = null;
+
+        try {
+
+            in = new Scanner(new File(this.usersFilePath));
+            String line;
+            while (in.hasNextLine()) {
+                lineBeingRead++;
+                line = in.nextLine();
+                String[] dados = line.split(",");
+
+                User newUser = new User(
+                        dados[0].replace(" ", ""),
+                        Integer.parseInt(dados[1]),
+                        dados[2].replace(" ", ""));
+
+                usersGraph.insertVertex(newUser);
+
+            }
+
+            return usersGraph;
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Ficheiro de dados não encontrado.");
+        } catch (NumberFormatException e) {
+            System.out.println("Erro ao ler a linha " +lineBeingRead+".");
+        } catch (Exception e) {
+            System.out.println("Erro ao ler o ficheiro.");
+        }finally {
+            if(in != null) in.close();
+        }
+        return usersGraph;
+    }
+
+    /**
+     * Carrega as relações entre utilizadores (ramos do grafo) a partir de um ficheiro .txt
+     */
+    private void  uploadRelationships(){
+
+        int initialEdges = usersGraph.numEdges();
+        int lineBeingRead = 0;
+        User vOrig;
+        User vDest;
+        Scanner in = null;
+        try {
+
+            in = new Scanner(new File(relationshipsFilePath));
+            String line;
+            while (in.hasNextLine()) {
+                lineBeingRead++;
+                line = in.nextLine();
+                String[] dados = line.split(",");
+
+                vOrig = null;
+                vDest = null;
+
+                //itera pelos vertices do grafo para verificar se os users existem no grafo
+                for(User vertice : usersGraph.vertices()){
+                    if(vertice.getUserId().equals(dados[0].replace(" ", ""))){
+                        vOrig = vertice;
+                    }
+                    if (vertice.getUserId().equals(dados[1].replace(" ", ""))) {
+                        vDest = vertice;
+                    }
+                }
+                //se encontrar os dois utilizadores no grafo adiciona a relação entre eles
+                if(vOrig != null && vDest != null){
+                    usersGraph.insertEdge(vOrig, vDest,  1.0);
+                }
+            }
+
+
+            if(usersGraph.numEdges() == initialEdges){
+                throw new IllegalArgumentException();
+            }
+
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Ficheiro de dados não encontrado.");
+        } catch (NumberFormatException e) {
+            System.out.println("Erro ao ler a linha " +lineBeingRead+".");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Não foram carregadas nenhumas relações.");
+        } catch (Exception e) {
+            System.out.println("Erro ao ler o ficheiro.");
+        }finally {
+            if(in != null) in.close();
+        }
+    }
+
+    /**
+     * Preenche os vértices do grafo de cidades (mapa de adjacências).
+     * Carrega,a partir de um ficheiro de texto, uma lista de países que vão ser adicionados como vertices.
+     * @return Graph Graph<V,E>
+     */
+    private Graph<Country,String> uploadCountries(){
+
+        int lineBeingRead = 0;
+        Scanner in = null;
+
+        try {
+
+            in = new Scanner(new File(this.countriesFilePath));
+            String line;
+            while (in.hasNextLine()) {
+                lineBeingRead++;
+                line = in.nextLine();
+                String[] dados = line.split(",");
+
+                Country newCountry = new Country(
+                        dados[0].replace(" ", ""),
+                        dados[1].replace(" ", ""),
+                        Double.parseDouble(dados[2]),
+                        dados[3].replace(" ", ""),
+                        Double.parseDouble(dados[4]),
+                        Double.parseDouble(dados[5]));
+                countriesGraph.insertVertex(newCountry);
+
+            }
+
+            return countriesGraph;
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Ficheiro de dados não encontrado.");
+        } catch (NumberFormatException e) {
+            System.out.println("Erro ao ler a linha " +lineBeingRead+".");
+        } catch (Exception e) {
+            System.out.println("Erro ao ler o ficheiro.");
+        }finally {
+            if(in != null) in.close();
+        }
+        return countriesGraph;
+    }
+
+
+    /**
+     * Cria os ramos do grafo a partir de de uma lista de fronteiras presentes num ficheiro .txt
+     * Ramos têm peso igual à distância, em Km, entre as capitais de cada país(Country).
+     */
+    private void  uploadBorders(){
+
+        int initialEdges = countriesGraph.numEdges();
+        int lineBeingRead = 0;
+        Country vOrig;
+        Country vDest;
+
+        Scanner in = null;
+        try {
+
+            in = new Scanner(new File(bordersFilePath));
+            String line;
+            while (in.hasNextLine()) {
+                lineBeingRead++;
+                line = in.nextLine();
+                String[] dados = line.split(",");
+
+                vOrig = null;
+                vDest = null;
+
+                //itera pelos vertices do grafo para verifica se os paises existem no grafo
+                for(Country pais : countriesGraph.vertices()){
+                    if(pais.getCountryName().equals(dados[0].replace(" ", ""))){
+                        vOrig = pais;
+                    }
+                    if (pais.getCountryName().equals(dados[1].replace(" ", ""))) {
+                        vDest = pais;
+                    }
+                }
+                //se encontrar os dois utilizadores(que estão na linha do ficheiro .txt) na lista de vertices do grafo
+                //então calcula a distancia entre as coordenadas e insere o ramo no grafo
+                if(vOrig != null && vDest != null){
+                    double distance = calculateDistanceBetween(vOrig, vDest);
+                    countriesGraph.insertEdge(vOrig, vDest,  "",distance);
+                }
+            }
+
+
+            if(countriesGraph.numEdges() == initialEdges){
+                throw new IllegalArgumentException();
+            }
+
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Ficheiro de dados não encontrado.");
+        } catch (NumberFormatException e) {
+            System.out.println("Erro ao ler a linha " +lineBeingRead+".");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Não foram carregadas nenhumas fronteiras.");
+        } catch (Exception e) {
+            System.out.println("Erro ao ler o ficheiro.");
+        }finally {
+            if(in != null) in.close();
+        }
+    }
+
+
+    /**
+     * Usa biblioteca externa para calcular a distância entre dois objetos Country
+     * @param country1 Country origem
+     * @param country2 Country destino
+     * @return double com a distância, em Km, entre o country1 e country2
+     */
+    private double calculateDistanceBetween(Country country1, Country country2){
+        GreatCircle globe = new GreatCircle();
+        Coordinate c1 = new Coordinate(country1.getLatitude(), country1.getLongitude());
+        Coordinate c2 = new Coordinate( country2.getLatitude(), country2.getLongitude());
+        return globe.distance(c1, c2);
+    }
+
+
+
+}
+
+
--- src/test/java/CarregarFicheirosTest.java
+++ src/test/java/CarregarFicheirosTest.java
@@ -1,18 +1,20 @@
+
+import TP2.*;
+import graph.AdjacencyMatrixGraph;
+import graphbase.Graph;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 import static org.junit.Assert.*;
-import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 
-import java.util.LinkedList;
-import java.util.List;
 
 
 @DisplayName("CarregarFicheirosTeste")
 public class CarregarFicheirosTest {
 
-    CarregarFicheiros carregarFicheiro = new CarregarFicheiros();
-    LinkedList<User> usersSCarregados = carregarFicheiro.uploadUsers();
-    LinkedList<Country> countriesSCarregados = carregarFicheiro.uploadCountries();
+    CarregarFicheiros carregarFicheiro = new CarregarFicheiros(true);
+
+    Graph<Country, String> countriesGraph = carregarFicheiro.carregarGrafoCountries();
+    AdjacencyMatrixGraph<User,Double> usersGraph = carregarFicheiro.carregarGrafoUsers();
 
 
     @Test
@@ -20,48 +22,81 @@ public class CarregarFicheirosTest {
     public void uploadUsersTest() {
         System.out.println("UploadUsersSmall");
 
-        LinkedList<User> expectedList = new LinkedList<>();
         User u1 = new User("u1", 27, "brasilia");
-        User u2 = new User("u2", 18, "lapaz");
-        User u3 = new User("u3", 20, "quito");
-        expectedList.add(u1);
-        expectedList.add(u2);
-        expectedList.add(u3);
+        assertTrue("checks if first user(first line from susers.txt) was uploaded",usersGraph.checkVertex(u1));
 
+        User u33 = new User("u33",48,"brasilia");
+        assertTrue("checks if last user(last line from susers.txt) was uploaded",usersGraph.checkVertex(u33));
 
-        List<User> resultList = usersSCarregados.subList(0, 3);
-        //testa os 3 primeiros elementos carregados
-        assertIterableEquals(expectedList, resultList);
-
-        User expectedUser = new User("u33", 48, "brasilia");
-        User resultUser = usersSCarregados.getLast();
-        //testa o ultimo elemento carregado
-        assertEquals(expectedUser, resultUser);
+        assertEquals("number of vertices loaded from susers.txt has to be 26",26,usersGraph.numVertices());
     }
+
 
 
     @Test
     @DisplayName("UploadCountriesSmall")
     public void uploadCountriesTest() {
         System.out.println("UploadCountriesSmall");
-        LinkedList<Country> expectedList = new LinkedList<>();
-        Country c1 = new Country("argentina", "americasul", 41.67, "buenosaires", -34.6131500, -58.3772300);
-        Country c2 = new Country("bolivia", "americasul", 9.70, "lapaz", -16.5000000, -68.1500000);
-        Country c3 = new Country("brasil", "americasul", 206.12, "brasilia", -15.7797200, -47.9297200);
 
-        expectedList.add(c1);
-        expectedList.add(c2);
-        expectedList.add(c3);
+        Country c1 = new Country("argentina","americasul",41.67,"buenosaires",-34.6131500,-58.3772300);
+        assertTrue("checks if first country(first line from scountries.txt) was uploaded",countriesGraph.validVertex(c1));
 
-        List<Country> resultList = countriesSCarregados.subList(0, 3);
-        //testa os 3 primeiros elementos
-        assertIterableEquals(expectedList, resultList);
+        Country c13 = new Country("uruguai", "americasul", 3.35, "montevideu", -34.9032800, -56.1881600);
+        assertTrue("checks if first country(first line from scountries.txt) was uploaded",countriesGraph.validVertex(c1));
+
+        assertEquals("number of vertices loaded from scountries.txt has to be 13",13,countriesGraph.numVertices());
+    }
+
+    @Test
+    @DisplayName("UploadRelationships")
+        public void uploadRelationships(){
+            System.out.println("uploadRelationshipsSmall");
+
+            User u1 = new User("u1", 27, "brasilia");
+            User u2 = new User("u2",18,"lapaz");
+
+            //'u2 ,u1' is the first line in srelationships so this Edge should already exist
+            assertFalse("this edge should already exist, thus the method should return false",usersGraph.insertEdge(u2, u1, 1.0));
 
 
-        Country expectedCountry = new Country("uruguai", "americasul", 3.35, "montevideu", -34.9032800, -56.1881600);
-        Country resultCountry = countriesSCarregados.getLast();
-        //testa o ultimo elemento carregado
-        assertEquals(expectedCountry, resultCountry);
+            User u33 = new User("u33",48,"brasilia");
+            User u32 = new User("u32",55,"brasilia");
+
+            //'u33 ,u32' is the last line(with a viable connection) in srelationships so this Edge should already exist
+            assertFalse("this edge should already exist, thus the method should return false",usersGraph.insertEdge(u33, u32, 1.0));
+
+            assertEquals("number of edges loaded from srelationships.txt has to be 45(out of 78 lines)",45,usersGraph.numEdges());
+        }
+
+    @Test
+    @DisplayName("UploadBorders")
+    public void uploadBorders(){
+        System.out.println("uploadBordersSmall");
+
+        assertEquals("number of edges loaded from sborders.txt has to be 50",50,countriesGraph.numEdges());
+
+
+        Country c1 = new Country("argentina","americasul",41.67,"buenosaires",-34.6131500,-58.3772300);
+        Country c2 = new Country("bolivia","americasul",9.70,"lapaz",-16.5000000,-68.1500000);
+
+        //'argentina, bolivia' is the first line in sborders so this Edge should already exist
+        assertTrue("The edge should already exist so the method should return true", countriesGraph.removeEdge(c1,c2));
+
+
+        Country c10 = new Country("suriname", "americasul", 0.04, "paramaribo", 5.8663800, -55.1668200);
+        Country c7 = new Country("guianafrancesa", "americasul", 2.88, "caiena", 4.9333300, -52.3333300);
+
+        //'guianafrancesa, suriname' is the last line in sborders so this Edge should already exist
+        assertTrue("The edge should already exist so the method should return true", countriesGraph.removeEdge(c7,c10));
+
+        //repõe a instancia original do grafo para poder ser usada nos restantes testes
+        carregarFicheiro.carregarGrafoCountries();
+    }
+
+    @Test
+    public void calculateDistanceBetween(){
+        //valores parecem divergir ligeiramente dos retornados no site
+        //http://www.movable-type.co.uk/scripts/latlong.html
     }
 
 }
